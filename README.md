f4a7df8e5c024fb7da05ad8454fe697584289ad7
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-06-28 17:40:05 +0200

    flask-blog

---
+++ README.md
@@ -0,0 +1,2 @@
+flask-blog
+this is a simple blog application built with flask following a tutorial by CoreySchafer.
---
+++ run.py
@@ -0,0 +1,9 @@
+from flaskblog import create_app, db
+
+app = create_app()
+
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+        # import json
+    app.run()
