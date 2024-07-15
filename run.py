4926a61342f9ac7ea74fe2ccf29fff7fed6bef3c
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-07-15 17:29:58 +0200

    modified run.py

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from web_app import create_app, db
+from web_app import create_app
 from web_app.models import User, Post
 from web_app import admin
 from flask_admin import Admin
@@ -9,7 +9,6 @@ app = create_app()
 
 if __name__ == "__main__":
     with app.app_context():
-        db.create_all()
         admin.add_view(AdminModelView(User, db.session))
         admin.add_view(AdminModelView(Post, db.session))
     app.run()
