d665d3d9a3a6a3fe5a187ba685e5da4b0b2f36fe
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-07-15 14:16:03 +0200

    modified config.py

--- web_app/config.py
+++ web_app/config.py
@@ -2,14 +2,14 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-db_user = os.getenv("BLOG_MYSQL_USER")
-db_pwd = os.getenv("BLOG_MYSQL_PWD")
-db_host = os.getenv("BLOG_MYSQL_HOST")
-db_name = os.getenv("BLOG_MYSQL_DB")
+db_user = os.getenv("BLOG_POSTGRES_USER")
+db_pwd = os.getenv("BLOG_POSTGRES_PWD")
+db_host = os.getenv("BLOG_POSTGRES_HOST")
+db_name = os.getenv("BLOG_POSTGRES_DB")
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql+psycopg2://{db_user}:{db_pwd}@{db_host}/{db_name}"
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
     MAIL_USE_TLS = True
