5d6695c17d63ff0367d34ac64c82017f46075a35
EmmanuelNiyonshuti <nemmy0257@gmail.com>
2024-07-20 12:29:09 +0200

    email verification

--- web_app/config.py
+++ web_app/config.py
@@ -2,17 +2,20 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-db_user = os.getenv("BLOG_MYSQL_USER")
-db_pwd = os.getenv("BLOG_MYSQL_PWD")
-db_host = os.getenv("BLOG_MYSQL_HOST")
-db_name = os.getenv("BLOG_MYSQL_DB")
+# db_user = os.getenv("BLOG_MYSQL_USER")
+# db_pwd = os.getenv("BLOG_MYSQL_PWD")
+# db_host = os.getenv("BLOG_MYSQL_HOST")
+# db_name = os.getenv("BLOG_MYSQL_DB")
 
-
+db_user = os.getenv("BLOG_POSTGRESQL_USER")
+db_pwd = os.getenv("BLOG_POSTGRESQL_PWD")
+db_host = os.getenv("BLOG_POSTGRESQL_HOST")
+db_name = os.getenv("BLOG_POSTGRESQL_DB")
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
-    # SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_pwd}@{db_host}/{db_name}"
+    # SQLALCHEMY_DATABASE_URI = f"mysql+pymysql://{db_user}:{db_pwd}@{db_host}/{db_name}"
+    SQLALCHEMY_DATABASE_URI = f"postgresql://{db_user}:{db_pwd}@{db_host}/{db_name}"
 
     MAIL_SERVER = "smtp.googlemail.com"
     MAIL_PORT = 587
